061bc41d19c4a1b274e5fbe14aa596e1f3bd4ef4
James Prentice <jamesprenticez@gmail.com>
2021-11-26 21:31:34 +1300

    Add left and right button

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -3,23 +3,46 @@ import { sliderItems } from '../data'
 
 function Slider() {
   const [img, setImg] = useState(1)
+  const [activeIndex, setIndex] = useState(0)
+
+  const goToPrevSlide = () => {
+    let index = activeIndex
+    let length = sliderItems.length
+    if(index < 1){
+      index = length -1
+    } else {
+      index--
+    }
+    setIndex(index)
+  }
+
+  const goToNextSlide = () => {
+    let index = activeIndex
+    let length = sliderItems.length
+    if(index === length - 1){
+      index = 0
+    } else {
+      index ++
+    }
+    setIndex(index)
+  }
 
   return (
     <>
         <div 
-          className="grid grid-cols-12"
+          className="grid grid-cols-12 h-screenNav"
           style={{backgroundColor: `${sliderItems[1].bg}`}}
         >
           <div className="h-[800px] col-start-2 col-span-10">
             <img 
               className="min-w-[600px] min-h-[800px] col-start-1 col-span-11"
-              src={sliderItems[img].src}
+              src={sliderItems[activeIndex].src}
             />
           </div>
 
           {/* Left Arrow */}
           <div 
-            onClick={() => console.log("Left")} 
+            onClick={() => goToPrevSlide()} 
             className="w-full col-start-1 col-span-1 row-start-1 flex items-center justify-center hover:cursor-pointer hover:bg-red-500 hover:bg-opacity-20 transistion ease-in-out duration-1000">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -33,7 +56,7 @@ function Slider() {
 
           {/* Right Arrow */}
           <div 
-            onClick={() => console.log("Right")} 
+            onClick={() => goToNextSlide()} 
             className="w-full col-start-12 col-span-1 row-start-1 flex items-center justify-center hover:cursor-pointer hover:bg-red-500 hover:bg-opacity-20 transistion ease-in-out duration-1000">
             <svg
               xmlns="http://www.w3.org/2000/svg"
